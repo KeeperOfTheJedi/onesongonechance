3ff529fb959a7edb4c9e69b629f0f85b12da74ac
oliver nadj <mr.oliver.nadj@gmail.com>
2016-04-26 11:18:30 +0000

    basic profile modal and callout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
   def my_profile
+    @user = current_user
     @sections = ["picture", "globe", "music", "friends"]
     unless params[:section].present?
       @section = "globe"
@@ -10,5 +12,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+
+  end
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
